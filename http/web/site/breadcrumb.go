c47cf40da51a8b4b58aff73b5d5c71487c4c6ed7
マリウス <marius@xn--gckvb8fzb.com>
2025-11-21 11:07:38 +0100

    Implement breadcrumbs, add draft hardcoded breadcrumbs

---
+++ http/web/site/breadcrumb.go
@@ -0,0 +1,29 @@
+package site
+
+type Breadcrumb struct {
+	IsActive bool
+	Label    string
+	Title    string
+	Href     string
+}
+
+func (p *Site) Breadcrumbs() []Breadcrumb {
+	return []Breadcrumb{
+		{
+			Label: "Cloud",
+			Title: "Cloud",
+			Href:  "", // TODO: Link
+		},
+		{
+			Label: "Hosting",
+			Title: "Hosting",
+			Href:  "", // TODO: Link
+		},
+		{
+			Label: "Opinions on Clouvider?",
+			Title: "Opinions on Clouvider?",
+			Href:  "", // TODO: Link
+			IsActive: true,
+		},
+	}
+}
